ca57c3dde30c091cc91323f824d44036be1231a2
shellshy <shellshy@sina.com>
2012-06-12 13:18:14 +0800

    ok

--- chaojia/chaojia/settings.py
+++ chaojia/chaojia/settings.py
@@ -66,7 +66,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/public/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
