cf2733e34b71c1e8acea73407020996955241c8d
shellshy <shellshy@sina.com>
2012-06-21 09:54:04 +0800

    修改应用的url

--- chaojia/chaojia/settings.py
+++ chaojia/chaojia/settings.py
@@ -133,6 +133,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chaojia',
+    'login',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
