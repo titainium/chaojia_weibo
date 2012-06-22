6f14191b735b2ab2f227f96b3404d4737bcdd482
shellshy <shellshy@sina.com>
2012-06-22 15:03:52 +0800

    merge

---
+++ chaojia/weibo/weiboUrl.py
@@ -0,0 +1,21 @@
+from django.conf.urls.defaults import patterns, include
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'bwsport.views.home', name='home'),
+    # url(r'^bwsport/', include('bwsport.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    (r'^$','weibo.views.weibo_home'),
+    (r'^$','weibo.views.weibo_home'),
+    (r'qzf/$','weibo.views.qzf'),
+    (r'qzf/choose/$','weibo.views.choose_weibo_zf'),
+)
