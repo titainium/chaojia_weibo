2f7ea0ec54fe95c427b4c5563ee8b02047d3be3d
lauly <475331388@qq.com>
2012-06-24 23:44:20 +0800

    Update master

--- chaojia/inform/models.py
+++ chaojia/inform/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class InformModel(models.Model):
+    wid = models.CharField(max_length=30);
+    uid = models.CharField(max_length=30);
+    inform_time = models.DateField();
+    reason = models.TextField(max_length=100);
+    status = models.IntegerField();
