7eae31954d84bbb549ba4789ea467e9c641adf40
shellshy <shellshy@sina.com>
2012-06-25 20:20:42 +0800

    代码添加try except

--- chaojia/weibo/provider/provider.py
+++ chaojia/weibo/provider/provider.py
@@ -10,19 +10,32 @@ WEIBO = redis.StrictRedis(host='localhost', port=6379, db=0)
 ADMIN = redis.StrictRedis(host='localhost', port=6379, db=9)
 
 def getTags():
-    tags = TAG.keys()
+    tags = None
+    try:
+        tags = TAG.keys()
+    except:
+        None    
     return tags
 
 def getUserList(tag):
-    uids = TAG.smembers(tag)
+    uids = None
+    try:
+        uids = TAG.smembers(tag)
+    except:
+        None
     return uids
 
 #获取用户标签
 def getUserTags(uid):
     result = []
     tags = getTags()
+    if tags == None:
+        return []
+    
     for tag in tags:
         uids = getUserList(tag)
+        if uids == None:
+            continue
         if uid in uids:
             result.append(tag)
     
@@ -32,8 +45,14 @@ def getUserTags(uid):
 def getRelateUsers(uid):
     result = []
     tags = getTags()
+    if tags == None:
+        return []
+    
     for tag in tags:
         uids = getUserList(tag)
+        if uids == None:
+            continue
+        
         if uid in uids:
             result = result + uids
     
@@ -43,25 +62,41 @@ def getRelateUsers(uid):
 
 #通过标签获取微博的id集合
 def getWeiboIdByTag(tag):
-    ids = WEIBO.smembers(tag + '_weiboids')
+    ids = None
+    try:
+        ids = WEIBO.smembers(tag + '_weiboids')
+    except:
+        None
     return ids
 
 #通过用户id获取微博的数据
 def getWeiboIdByUid(uid):
-    ids = WEIBO.smembers(uid + '_weiboids')
+    ids = None
+    try:
+        ids = WEIBO.smembers(uid + '_weiboids')
+    except:
+        None
     return ids
 
 #通过微博的id获取微博的详细信息
 def getWeiboById(wid):
-    weibo = WEIBO.get("weiboid_" + wid)
+    weibo = None
+    try:
+        weibo = WEIBO.get("weiboid_" + wid)
+    except:
+        None
     return weibo
 
 #用户获取置顶的weibo数据
 def getZhiDingWid(uid):
+    result = []
     zhiding_wids = ADMIN.get['zhiding'].values()#获取置顶的微博id    
     keys = ADMIN.keys()#三个小时用户转发过的微博wid
     count = 0
-    result = []
+    
+    if zhiding_wids == None:
+        return result
+    
     for wid in zhiding_wids:
         if count > 3:
             break;
